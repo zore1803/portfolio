0ba727bcc338676bf8b6f67abe3f6054c5112c24
zore1803 <rzore5104@gmail.com>
2026-05-03 20:50:17 +0530

    Fix: Removed custom cursor to improve performance and remove lag

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider>
       <TooltipProvider>
-        <CustomCursor />
         <Toaster />
         <Sonner />
         <BrowserRouter>
